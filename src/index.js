dd4ede6c502a3310928b673ae10883422f531662
Rio Trilaksono Putro <riotrilaksonop@gmail.com>
2021-03-27 14:18:49 +0700

    feat: learn axios instance

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import './index.css';
 
 axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
-axios.defaults.headers.common['Authorization'] = 'AUTH_TOKEN';
+// axios.defaults.headers.common['Authorization'] = 'AUTH_TOKEN';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 axios.interceptors.request.use(
